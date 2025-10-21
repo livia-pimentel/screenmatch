40de539a42202ef6a4a0d26b13c02937d61f3ac2
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-10-21 15:49:53 -0300

    Add getter, setters e alteração de modificadores de visibilidade

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,11 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Top Gun";
-        meuFilme.anoDeLancamento = 2022;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Top Gun");
+        meuFilme.setAnoDeLancamento(2022);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
