e55c1535e852023153f2949f5f8bb6f632f5baa6
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-10-20 22:32:16 -0300

    Initial commit

---
+++ src/Filme.java
@@ -0,0 +1,13 @@
+public class Filme {
+    // Atributos
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacao;
+    int duracaoEmMinutos;
+
+    
+
+
+}
