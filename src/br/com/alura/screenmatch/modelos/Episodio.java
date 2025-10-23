006dc79c314525acc612d6d11a73f47d42e2bfdd
livia-pimentel <livia_macedo89@yahoo.com.br>
2025-10-23 09:49:07 -0300

    Implementação de Interface

--- src/br/com/alura/screenmatch/modelos/Episodio.java
+++ src/br/com/alura/screenmatch/modelos/Episodio.java
@@ -1,10 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Episodio {
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Episodio implements Classificavel {
     // Atributos
     private int numero;
     private String nome;
     private Serie serie;
+    private int totalVisualizacoes;
 
     // Getters e Setters
     public int getNumero() {
@@ -29,5 +32,22 @@ public class Episodio {
 
     public void setSerie(Serie serie) {
         this.serie = serie;
+    }
+
+    public int getTotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    @Override
+    public int getClassificacao() {
+        if (totalVisualizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
     }
 }
